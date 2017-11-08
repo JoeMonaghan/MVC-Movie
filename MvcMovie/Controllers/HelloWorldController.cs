78f0d706c0493cd22c164289af355aa9155f8dc3
Joe-Monaghan <JoeMonaghandev@outlook.com>
2017-11-08 19:56:18 +0000

    Hello World Controller added

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // Each Controllers public method is an a endpoint.
+        // for example Welcome() will have HelloWorld/Welcome
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int id = 1)
+        {
+            // 
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        }
+    }
+}
